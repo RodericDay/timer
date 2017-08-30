496dcaf1b1fc5b1b21482f6c45d36c478e736b6a
Roderic Day <roderic.day@gmail.com>
2017-08-30 12:55:02 -0400

    Latest

--- clock.js
+++ clock.js
@@ -1,54 +1,25 @@
 var audioCtx = new (window.AudioContext || window.webkitAudioContext);
 
 class Sound {
-
-    constructor(path)
-    {
+    constructor(path) {
         var xhr = new XMLHttpRequest();
         xhr.open('GET', path, true);
         xhr.responseType = 'arraybuffer';
         xhr.addEventListener("load", this.decode.bind(this));
         xhr.send();
     }
-
-    decode(event)
-    {
+    decode(event) {
         audioCtx.decodeAudioData(event.target.response, this.buffer.bind(this));
     }
-
-    buffer(decodedBuffer)
-    {
+    buffer(decodedBuffer) {
         this.bufferedSound = decodedBuffer;
     }
-
-    play()
-    {
+    play() {
         var source = audioCtx.createBufferSource();
         source.buffer = this.bufferedSound;
         source.connect(audioCtx.destination);
         source.start(0);
     }
-
-}
-
-var tic = new Sound('sound_tic.mp3');
-var bell = new Sound('sound_bell.mp3');
-var cheer = new Sound('sound_cheer.wav');
-
-var interval = window.setInterval(update, 50);
-var constants = {
-    setCount: 20,
-    setLength: 30,
-    bufferLength: 5,
-    speed: 1,
-}
-
-var state = {
-    last: new Date(),
-    active: false,
-    setsLeft: constants.setCount,
-    secondsLeft: constants.bufferLength,
-    millisecondsLeft: 0,
 }
 
 function update() {
@@ -56,23 +27,20 @@ function update() {
     if (state.active) {
         state.millisecondsLeft -= (tt - state.last);
         if (state.millisecondsLeft < 0) {
+            if (state.secondsLeft % 3 == 0) { tic.play(); }
             state.millisecondsLeft += 1000/constants.speed;
             state.secondsLeft -= 1;
-            if (state.secondsLeft < 5) {
-                tic.currentTime = 0;
-                tic.play();
-            }
         }
         if (state.secondsLeft < 0) {
             state.secondsLeft += constants.setLength;
             state.setsLeft -= 1;
-            bell.currentTime = 0;
             bell.play();
         }
         if (state.setsLeft < 0) {
             cheer.play();
-            clearInterval(interval);
+            state.active = false;
             document.title = 'Timer';
+            reset();
             return
         }
     }
@@ -82,43 +50,53 @@ function update() {
 
 function updateDisplay() {
     /* fake regular timer display from arbitary state */
-    var A = parseInt(state.setsLeft/2);
-    var B = 2*A === state.setsLeft ? 0 : 30;
-    var C = state.secondsLeft+B;
-    var D = parseInt(state.millisecondsLeft/100);
-    var formatted = A+":"+('00'+C).slice(-2)+'.'+D;
+    var remaining = state.secondsLeft+state.millisecondsLeft/1000;
+    var formatted = parseInt(state.setsLeft/2)+':'+('00'+remaining.toFixed(1)).slice(-4);
     document.title = state.active ? formatted : "Timer";
     /* ui state */
-    var r = 125,
-        f = (state.secondsLeft + state.millisecondsLeft/1000)/constants.setLength,
-        t = Math.PI/180 * f*360,
-        l = t < Math.PI ? 0 : 1,
-        x = r*Math.cos(-t),
-        y = r*Math.sin(-t);
+    var r = 128,
+        f = remaining / constants.setLength,
+        l = f > 0.5 ? 1 : 0,
+        t = f * (2 * Math.PI),
+        x = -r * Math.sin(t),
+        y = -r * Math.cos(t);
     /*
-    * move to 0,0
-    * move up r
-    * do a sweep r,r
-    * rotate 45 cause clocks have top @ 90d
-    * toggle large or small arc as required
-    * end at x, y
+    * move to 0,0; move up r; do a sweep r,r;
+    * toggle large or small arc as required; end at x, y;
     */
-    var anim = `M0,0 h${r} A${r},${r},0,${l},0,${x},${y} z`;
+    var anim = `M0,0 v${-r} A${r},${r},0,${l},0,${x},${y} z`;
     loader.setAttribute('d', anim);
     border.setAttribute('d', anim);
 
-    display.textContent = formatted;
+    display.textContent = state.active ? formatted :  "-:--.-";
     actionButton.textContent = state.active ? "stop" : "start";
     resetButton.disabled = state.active;
 }
 
-function toggle(button) {
+function toggle() {
     state.active = !state.active;
 }
 
 function reset() {
-    state.active = false;
-    state.setsLeft = constants.setCount;
-    state.secondsLeft = constants.bufferLength;
-    state.millisecondsLeft = 0;
+    state = {
+        last: new Date(),
+        active: false,
+        setsLeft: constants.setCount,
+        secondsLeft: constants.bufferLength,
+        millisecondsLeft: 0,
+    }
 }
+
+var state;
+var tic = new Sound('sound_tic.mp3');
+var bell = new Sound('sound_bell.mp3');
+var cheer = new Sound('sound_cheer.wav');
+var constants = {
+    setCount: 20,
+    setLength: 30,
+    bufferLength: 3,
+    speed: 1.00,
+}
+
+reset();
+setInterval(update, 1000/30);
